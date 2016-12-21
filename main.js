44f6f9e163f8fe532bc9c27d6fd9af016b1a7e43
Pedro Aim <pedro_aim@hotmail.com>
2016-12-21 10:25:03 -0500

    reconnect start

--- main.js
+++ main.js
@@ -223,15 +223,8 @@ require('es6-promise').polyfill();
         this.aliveCounter = 3
         let storedMonkeyId = db.getMonkeyId();
         this.ping();
-        //if no user logged in, do nothing
-        if (storedMonkeyId == null || storedMonkeyId === '') {
-          return;
-        }
-
-        if (this.socketConnection == null && storedMonkeyId != null && storedMonkeyId !== '') {
-          this.startConnection();
-          return;
-        }
+        this.startConnection();
+        
       }.bind(this)
 
       xhr.send();
