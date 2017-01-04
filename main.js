668e988a8b3817d36fb48c32e06c0ef9a4177e04
Gianni Carlo <gcarlo89@hotmail.com>
2017-01-04 17:45:00 -0500

    Fix https

--- main.js
+++ main.js
@@ -146,7 +146,7 @@ require('es6-promise').polyfill();
     }
 
     this.session.autoSave = autoSave || true;
-    this.domainUrl = '35.165.188.149:8080';
+    this.domainUrl = 'secure.criptext.com';
     this.session.ignore = ignoreHook;
 
     if (isStaging) {
@@ -210,7 +210,7 @@ require('es6-promise').polyfill();
   proto.checkConnectivity = function checkConnectivity(){
     if(!this.internet){
       var xhr = new ( window.ActiveXObject || XMLHttpRequest )( "Microsoft.XMLHTTP" );
-      xhr.open( "GET", "http://" + this.domainUrl + "/ping" , true);
+      xhr.open( "GET", "https://" + this.domainUrl + "/ping" , true);
 
       xhr.onerror = function (e){
         if (this.socketConnection != null) {
@@ -1010,12 +1010,12 @@ require('es6-promise').polyfill();
     this._getEmitter().emit(STATUS_CHANGE_EVENT, this.status);
     let token=this.appKey+":"+this.appSecret;
 
-    // if(this.session.stage){ //no ssl
+    if(this.session.stage){ //no ssl
       this.socketConnection = new WebSocket('ws://'+this.domainUrl+'/websockets?monkey_id='+monkey_id+'&p='+token,'criptext-protocol');
-    // }
-    // else{
-    //   this.socketConnection = new WebSocket('wss://'+this.domainUrl+'/websockets?monkey_id='+monkey_id+'&p='+token,'criptext-protocol');
-    // }
+    }
+    else{
+      this.socketConnection = new WebSocket('wss://'+this.domainUrl+'/websockets?monkey_id='+monkey_id+'&p='+token,'criptext-protocol');
+    }
 
     this.socketConnection.onopen = function () {
       this.status=this.enums.Status.ONLINE;
